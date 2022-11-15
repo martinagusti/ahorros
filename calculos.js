e4bf912a1d2c89a608284c12a30c99fd0aeb636c
Martin Agusti <70919001+martinagusti@users.noreply.github.com>
2022-11-15 01:38:55 +0100

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,40 @@
+let importeInic = document.getElementById("importeInic")
+let depMens = document.getElementById("depMens")
+let extras = document.getElementById("extras")
+let tasa = document.getElementById("tasa")
+let años = document.getElementById("años")
+let dinero = 0
+
+let boton = document.getElementById("boton")
+
+
+    
+boton.addEventListener("click", calcular)
+
+
+
+  function calcular() {
+    dinero = (parseInt(importeInic.value) + (parseInt(depMens.value) * 12) +(parseInt(extras.value)))* ((parseInt(tasa.value)/100)+1)
+    console.log(dinero.toFixed(2))
+
+    for(let i = 1; i < parseInt(años.value); i++){
+
+        dinero = (dinero + (parseInt(depMens.value) * 12) +(parseInt(extras.value)))* ((parseInt(tasa.value)/100)+1)
+        console.log(dinero.toFixed(2))
+        
+    }
+    
+    
+ }
+
+
+ 
+
+
+ 
+
+ 
+ 
+
+
+  
